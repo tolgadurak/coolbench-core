24254691b2ad97828f1c6f4cf25af2f805cabcc2
Tolga Durak <dev.tolga@gmail.com>
2018-03-12 00:52:18 +0300

    Some refactorings

--- src/main/java/io/tolgadurak/coolbench/counter/CoolbenchPerformanceCounter.java
+++ src/main/java/io/tolgadurak/coolbench/counter/CoolbenchPerformanceCounter.java
@@ -1,4 +1,4 @@
-package io.tolgadurak.coolbench.main;
+package io.tolgadurak.coolbench.counter;
 
 import java.util.concurrent.TimeoutException;
 
@@ -13,23 +13,14 @@ public class CoolbenchPerformanceCounter {
 
 	private static final Logger logger = LogManager.getLogger(CoolbenchPerformanceCounter.class);
 
-	public static void main(String[] args) throws InterruptedException {
-		CoolbenchPerformanceCounter counter = new CoolbenchPerformanceCounter();
-		BenchmarkAlgorithm algorithm = BenchmarkAlgorithm.getInstance("SHA-512");
-		// This will perform multi core hashing task
-		long score = counter.benchmark(33554432, 8, algorithm);
-		logger.info("Performance score: " + score);
-		
-	}
-
-	public long benchmark(int nTimes, int nTasks, BenchmarkAlgorithm benchmarkAlgorithm) throws InterruptedException {
+	public long benchmark(int nTimes, int nThreads, BenchmarkAlgorithm benchmarkAlgorithm) throws InterruptedException {
 		boolean done = false;
 		long startTime = System.nanoTime();
 		Timeout timeout = Timeout.getInstance(benchmarkAlgorithm);
 		ScoreCalculator scoreCalculator = new ScoreCalculator();
 		logger.debug("Benchmark has been started");
 		try {
-			benchmarkAlgorithm.run(nTimes, nTasks, timeout);
+			benchmarkAlgorithm.run(nTimes, nThreads, timeout);
 		} catch (TimeoutException e) {
 			logger.error("Timeout is exceeded while performing benchmark");
 		}
@@ -37,8 +28,8 @@ public class CoolbenchPerformanceCounter {
 		long endTime = System.nanoTime();
 		long duration = (endTime - startTime);
 		long durationInMillis = duration / 1000000;
-		logger.info("duration - " + durationInMillis + "ms");
-		logger.info("Done within timeout: " + done);
+		logger.debug("duration - " + durationInMillis + "ms");
+		logger.debug("Done within timeout: " + done);
 		return scoreCalculator.doAction(durationInMillis);
 	}
 }
